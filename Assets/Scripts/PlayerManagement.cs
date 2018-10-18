d6f080bc737aeb64158371141973718c10ee6f9f
Yiren Wang <yw4062@nyu.edu>
2018-10-18 00:33:34 -0400

    Add some mushrooms and scripts to pick up the mushroom.

---
+++ Assets/Scripts/PlayerManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManagement : MonoBehaviour
+{
+	private float curMushroom;
+	public float MushroomNumGoal;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		curMushroom = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddMushroom()
+	{
+		curMushroom++;
+		Debug.Log("Number of mushrooms:" + curMushroom);
+		if (curMushroom >= MushroomNumGoal)
+		{
+			Debug.Log("You have enough mushrooms!");
+		}
+	}
+}
