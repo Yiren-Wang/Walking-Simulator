81ec891b21fdaa6769190b6d14d1f0af3e0777d6
Yiren Wang <yw4062@nyu.edu>
2018-10-26 10:51:20 -0400

    Make the boat move

---
+++ Assets/Scripts/BoatMoving.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class BoatMoving : MonoBehaviour {
+
+	public Transform target;
+	public PathType pathType = PathType.CatmullRom;
+	public Transform[] waypoints;
+
+
+	void Start()
+	{
+		Vector3[] points = new Vector3[waypoints.Length];
+		for (int i = 0; i < waypoints.Length; i++)
+		{
+			points[i] = waypoints[i].position;
+		}
+		// Create a path tween using the given pathType, Linear or CatmullRom (curved).
+		// Use SetOptions to close the path
+		// and SetLookAt to make the target orient to the path itself
+		
+		Tween rotate = target.DOShakeRotation(5, 1.5f, 2, 120f, false);
+		rotate.SetLoops(-1);
+		Tween t = target.DOPath(points, 20, pathType)
+			.SetOptions(false)
+			.SetLookAt(1f);
+		// Then set the ease to InOutQuad and use infinite loops
+		t.SetEase(Ease.InOutQuad);
+
+		
+	}
+}
